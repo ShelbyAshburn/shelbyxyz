276152baf1eb7641ce3bb612a1469df8bd2b0ef9
Shelby Ashburn <76406378+ShelbyAshburn@users.noreply.github.com>
2025-04-05 22:04:00 -0400

    No portfolio, clock working

--- startbootstrap-one-page-wonder-gh-pages/js/scripts.js
+++ startbootstrap-one-page-wonder-gh-pages/js/scripts.js
@@ -5,3 +5,17 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+/*Clock on the apps.html page*/ 
+function display_c(){
+    var refresh=1000; // Refresh rate in milli seconds
+    mytime=setTimeout('display_ct()',refresh)
+ }
+ 
+ function display_ct() {
+   var CDate = new Date()
+   var NewDate=CDate.toDateString(); 
+   NewDate = NewDate + " - " + CDate.toLocaleTimeString();
+   document.getElementById('ct').innerHTML = NewDate;
+   display_c();
+ }
